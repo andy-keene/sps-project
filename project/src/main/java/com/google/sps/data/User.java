2352709aba22540abb00373b6a2ed5c5587e52ee
MaleeS <m.seechan@gmail.com>
2021-07-01 04:53:44 +0000

    Added UserClass and servlet to create new user entities

---
+++ project/src/main/java/com/google/sps/data/User.java
@@ -0,0 +1,16 @@
+package com.google.sps.data;
+
+public final class User {
+    private final long id;
+    private final String userEmail;
+    private final String userName; 
+    private final String userPassword;
+  
+    //public User(long id, String userName, String userEmail, String userPassword)
+     public User(long id, String userName, String userEmail, String userPassword) {
+        this.id = id;
+        this.userName = userName; 
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+    }
+}
---
+++ project/src/main/java/com/google/sps/servlets/CreateAccountServlet.java
@@ -0,0 +1,52 @@
+package com.google.sps.servlets;
+
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+
+@WebServlet("/Create-Account")
+public class CreateAccountServlet<User> extends HttpServlet {
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // get parameters/user input for account creation
+        String userEmail = request.getParameter("Email Address");
+        String userName = request.getParameter("Username");
+        String userPassword = request.getParameter("password");
+      
+        // clean inputs
+        if (userEmail != null) {
+            userEmail = Jsoup.clean(userEmail, Whitelist.none());
+        }
+         if (userName != null) {
+            userName = Jsoup.clean(userName, Whitelist.none());
+        }
+        if (userPassword != null) {
+            userPassword = Jsoup.clean(userPassword, Whitelist.none());
+        }
+        
+        // Sets kind and build User Entity
+        Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+        KeyFactory keyFactory = datastore.newKeyFactory().setKind("User");
+        FullEntity userEntity = Entity.newBuilder(keyFactory.newKey())
+            .set("Email Address", userEmail)
+            .set("Username", userName)
+            .set("password", userPassword)
+            .build();
+        datastore.put(userEntity);
+        
+        // Print values in server logs for testing
+        System.out.println("Email: " + userEmail + "\nUsername: " + userName + "\nPassword: " + userPassword);
+
+        response.sendRedirect("/index.html");  //not sure where to redirect yet
+    }
+}
