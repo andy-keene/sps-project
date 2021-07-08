a62e739e2db8827814bd0ea3ab11db87f81bdbd9
Sumanjit <sumanjit.gill11@gmail.com>
2021-07-08 20:02:21 +0000

    Modify Routine class to contain a routine type so routines for specific body parts can be identified from datastore

--- project/src/main/java/com/google/sps/servlets/Routine.java
+++ project/src/main/java/com/google/sps/servlets/Routine.java
@@ -4,13 +4,14 @@ import java.util.ArrayList;
 
 public class Routine {
 
-    String title, description;
+    String title, description, type;
     ArrayList<String> moveSet;
     int sets;
 
-    public Routine(String routineTitle, String routineDescription, ArrayList<String> routineMoveSet, int routineSets) {
+    public Routine(String routineTitle, String routineDescription, String routineType, ArrayList<String> routineMoveSet, int routineSets) {
         title = routineTitle;
         description = routineDescription;
+        type = routineType;
         moveSet = routineMoveSet;
         sets = routineSets;
     }
--- project/src/main/java/com/google/sps/servlets/predefinedRoutineServlet.java
+++ project/src/main/java/com/google/sps/servlets/predefinedRoutineServlet.java
@@ -25,36 +25,36 @@ public class predefinedRoutineServlet extends HttpServlet {
                 Arrays.asList("Dumbbell Swings", "Dumbbell Jack", "Burpee Front Raise", "Plank Jack",
                         "Skater Dumbbell Toss", "Weighted Squat to Lunge", "Plank Jack Row"));
         Routine cardio = new Routine("Head to Toe Cardio",
-                "Do each move for 45 seconds and rest after each move for 10 seconds.", cardioMoves, 3);
+                "Do each move for 45 seconds and rest after each move for 10 seconds.", "Cardio", cardioMoves, 3);
 
         ArrayList<String> armsMoves = new ArrayList<String>(
                 Arrays.asList("Shoulder Press With Lunge (Right Leg)", "Elevated Bicep Curls With Lunge (Left Leg)",
                         "Reverse Flyes", "Tricep Pressbacks", "Plank Row", "Prayer Pulse", "Upright Row"));
         Routine arms = new Routine("Extreme Arm Toner",
-                "Do each move for 45 seconds and rest after each move for 15 seconds.", armsMoves, 3);
+                "Do each move for 45 seconds and rest after each move for 15 seconds.", "Arms", armsMoves, 3);
 
         ArrayList<String> legsMoves = new ArrayList<String>(
                 Arrays.asList("Side Lunges (Right Leg)", "Releve Chairs", "Side Lunges (Left Leg)",
                         "Releve Plie Pulses", "Squat Lunges", "Leg Circles (Right Leg)", "Leg Circles (Left Leg)"));
         Routine legs = new Routine("Leg Shape and Tone",
-                "Do each move for 1 minute and rest after each move for 15 seconds.", legsMoves, 3);
+                "Do each move for 1 minute and rest after each move for 15 seconds.", "Legs", legsMoves, 3);
 
         ArrayList<String> abMoves = new ArrayList<String>(Arrays.asList("One Arm Rollup", "Weighted Russian Twists",
                 "Weighted Toe Reach", "Weighted Knee Drives (Left Leg)", "Weighted Knee Drives (Right Leg)",
                 "Side Plank Twists (Left Leg)", "Side Plank Twists (Right Leg)"));
         Routine abs = new Routine("Extreme Ab Toner",
-                "Do each move for 1 minute and rest for 15 seconds after each move.", abMoves, 3);
+                "Do each move for 1 minute and rest for 15 seconds after each move.", "Abs", abMoves, 3);
 
         ArrayList<String> glutesMoves = new ArrayList<String>(Arrays.asList("Crab Walks",
                 "Straight Leg Lift (Right Leg)", "Pointed Butt Lift (Right Leg)", "Fire Hydrant (Right Leg)",
                 "Straight Leg Lift (Left Leg)", "Pointed Butt Lift (Left Leg)", "Fire Hydrant (Left Leg)"));
         Routine glutes = new Routine("Glute Shape and Tone",
-                "Do each move for 1 minute and rest for 30 seconds after each set", glutesMoves, 3);
+                "Do each move for 1 minute and rest for 30 seconds after each set", "Glutes", glutesMoves, 3);
 
         ArrayList<String> backShoulderMoves = new ArrayList<String>(Arrays.asList("Walnut Crushers", "Bird Dog Pulses",
                 "Parachuter", "Flying Bird Dog", "Breast Stroke", "Grasshopper"));
         Routine backShoulder = new Routine("Ultimate Back/Shoulder Workout",
-                "Do each move for 45 seconds and rest for 15 seconds between each move.", backShoulderMoves, 5);
+                "Do each move for 45 seconds and rest for 15 seconds between each move.", "Back/Shoulder", backShoulderMoves, 5);
 
         ArrayList<Routine> routines = new ArrayList<Routine>(Arrays.asList(cardio, arms, legs, abs, glutes, backShoulder));
 
