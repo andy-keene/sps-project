1480534c4b57dd1c31b70b77dfbdaf75ea3ea779
MaleeS <m.seechan@gmail.com>
2021-08-11 22:09:07 +0000

    create account saves salt and hashed pw. unable to retrieve salt to recreate hashed pw for comparison

--- project/src/main/java/com/google/sps/data/User.java
+++ project/src/main/java/com/google/sps/data/User.java
@@ -5,12 +5,16 @@ public final class User {
     private final String userEmail;
     private final String userName; 
     private final String userPassword;
+    private final String passwordHash;
+    private final String passwordSalt;
 
-    public User(long id, String userName, String userEmail, String userPassword) {
+    public User(long id, String userName, String userEmail, String userPassword, String passwordHash, String passwordSalt) {
         this.id = id;
         this.userName = userName; 
         this.userEmail = userEmail;
         this.userPassword = userPassword;
+        this.passwordHash = passwordHash;
+        this.passwordSalt = passwordSalt;
     }
 
     public long getuserId() {
@@ -27,5 +31,13 @@ public final class User {
 
     public String getuserPassword() {
         return userPassword;
+    }
+
+    public String getHash() {
+        return passwordHash;
+    }
+
+    public String getSalt() {
+        return passwordSalt;
     }
 }
--- project/src/main/java/com/google/sps/servlets/userLoginServlet.java
+++ project/src/main/java/com/google/sps/servlets/userLoginServlet.java
@@ -9,6 +9,11 @@ import com.google.cloud.datastore.StructuredQuery.PropertyFilter;
 import com.google.gson.Gson;
 import com.google.sps.data.User;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -37,20 +42,52 @@ public class userLoginServlet extends HttpServlet {
 
         // Checks query results to see if an entity exists based on the username or
         // email given.
-        // It then checks if the input password matches the password associated with the
-        // found entity.
+        // Hashes the salted input password and checks if the new hash matches the hash
+        // stored in the database.
         if (queryResults.hasNext()) {
+
             String responseId;
             Entity user = queryResults.next();
-            String pass = user.getString("signupPassword").replaceAll("\"", "");
+            // String pass = user.getString("signupPassword").replaceAll("\"", "");
             String userId = user.getKey().getId().toString();
+            String hashedPassword = user.getString("hashedPassword");
+            String salt = user.getString("salt");
 
-            // Checks if input and datastore passwords match.
-            if (pass.equals(passwordInput)) {
-                response.setContentType("application/json;");
-                responseId = "{\"userId\":" + userId + " }";
-                response.getWriter().println(responseId);
+            System.out.println("load hashpw: " + hashedPassword + " load salt: " + salt);
+
+            try {
+                /*
+                 * Checks if input and datastore passwords match. if
+                 * (pass.equals(passwordInput)) { response.setContentType("application/json;");
+                 * responseId = "{\"userId\":" + userId + " }";
+                 * response.getWriter().println(responseId); }
+                 */
+
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA-256");
+
+                // use salt from database .getBytes() doesn't do what i'm thinking it does
+                md.update(salt.getBytes());
+                byte[] hash = md.digest(passwordInput.getBytes(StandardCharsets.UTF_8));
+                StringBuilder newHashedPassword = new StringBuilder();
+                for (byte b : hash) {
+                    newHashedPassword.append(String.format("%02x", b));
+                }
+
+                // system out to see if salt is loading correctly (it isn't)
+                System.out.println("salt array: " + salt.getBytes().toString() + "  salt getB: " + salt.getBytes());
+                System.out.println("new hashpw: " + newHashedPassword.toString());
+
+                if (hashedPassword.equals(newHashedPassword.toString())) {
+                    response.setContentType("application/json;");
+                    responseId = "{\"userId\":" + userId + " }";
+                    response.getWriter().println(responseId);
+                }
+
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
             }
         }
+
     }
 }
